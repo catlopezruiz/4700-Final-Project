680c4845e1afce695aa776aa45b222a87f227f8d
hdlavine-web <hdlavine@icloud.com>
2026-04-14 17:29:28 -0700

    added a main menu

---
+++ Assets/bowlingSceneEnter1.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class bowlingSceneEnter1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void LoadByIndex()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
